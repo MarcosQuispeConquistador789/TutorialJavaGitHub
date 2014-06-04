1b82405be4843ec1252791818318f7e51b9f6c46
conquistador789Marcos <Marcos@Marcos-PC>
2014-06-04 09:36:33 -0400

    codigo hash

--- src/appengithub/AppEnGitHub.java
+++ src/appengithub/AppEnGitHub.java
@@ -15,6 +15,7 @@ public class AppEnGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        String sms="Hola mundo";        
+        System.out.println("hash code: "+sms.hashCode());
     }
 }
